516a9a647f22774a0698fda5fb42a6798fdfd47c
Olivier MARTIN <pixelduck.game@gmail.com>
2020-12-02 09:52:27 +0100

    Create javascript.js

---
+++ wat/javascript.js
@@ -0,0 +1,14 @@
+// UTF16
+{
+  let text = "𠮷𠮷";
+  console.log(text.length); 
+}
+
+// let
+{
+  console.log(typeof value);
+  if (true){
+	  console.log(typeof value);
+	  let value = 'wat';
+  }
+}
